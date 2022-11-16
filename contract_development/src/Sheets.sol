e302f5c75eb0743e144927f065f7ef731292e98d
Ethan Davis <daviseo@dukes.jmu.edu>
2022-11-15 20:26:22 -0500

    completed contract testing

--- contract_development/src/Sheets.sol
+++ contract_development/src/Sheets.sol
@@ -1,58 +1,65 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "openzeppelin-contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "openzeppelin-contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "openzeppelin-contracts/utils/Counters.sol";
 
 contract Sheets is ERC721, ERC721Enumerable, ERC721URIStorage {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    mapping(uint256 => string[]) private _authors;
+    mapping(uint256 => string[]) private _contributors;
     mapping(uint256 => string) private _title;
 
     constructor() ERC721("Sheets", "SHT") {}
 
-    function safeMint(
-        string[] memory authors, 
-        string memory title,
-        string memory uri
-    ) public {
-        uint256 tokenId = _tokenIdCounter.current();
+    function mint(string[] memory contributors, string memory title, string memory uri)
+        public returns(uint256 tokenId)
+    {
+        tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
-        _authors[tokenId] = authors;
+
+        _contributors[tokenId] = contributors;
         _title[tokenId] = title;
         _safeMint(msg.sender, tokenId);
         _setTokenURI(tokenId, uri);
+
+        return tokenId;
     }
 
-    function safeMint(
-        string memory author, 
-        string memory title,
-        string memory uri
-    ) public {
-        uint256 tokenId = _tokenIdCounter.current();
+    function mint(string memory contributor, string memory title, string memory uri) 
+        public returns(uint256 tokenId)
+    {
+        tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
-        _authors[tokenId][0] = author;
+
+        _contributors[tokenId] = [contributor];
         _title[tokenId] = title;
         _safeMint(msg.sender, tokenId);
         _setTokenURI(tokenId, uri);
+
+        return tokenId;
     }
 
-    function authors(uint256 tokenId) external view returns(string[]) {
-        return _authors[tokenId];
+    function getContributors(uint256 tokenId) public view returns(string[] memory) {
+        return _contributors[tokenId];
     }
 
-    function title(uint256 tokenId) external view returns(string) {
+    function getTitle(uint256 tokenId) public view returns(string memory) {
         return _title[tokenId];
     }
 
+    function tokenURI(uint256 tokenId)
+        public view override(ERC721, ERC721URIStorage) returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
-        internal
-        override(ERC721, ERC721Enumerable)
+        internal override(ERC721, ERC721Enumerable)
     {
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
@@ -61,20 +68,8 @@ contract Sheets is ERC721, ERC721Enumerable, ERC721URIStorage {
         super._burn(tokenId);
     }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
-
     function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
+        public view override(ERC721, ERC721Enumerable) returns (bool)
     {
         return super.supportsInterface(interfaceId);
     }
--- contract_development/test/Sheets.t.sol
+++ contract_development/test/Sheets.t.sol
@@ -3,20 +3,68 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/Sheets.sol";
+import "openzeppelin-contracts/token/ERC721/IERC721Receiver.sol";
 
-contract SheetsTest is Test {
+contract SheetsTest is Test, IERC721Receiver {
     Sheets public sheets;
     string public ipfsPath;
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
     
     function setUp() public {
         sheets = new Sheets();
         ipfsPath = "QmaBCkhaeaaCzEKK1K9xzL39vMaXjA6QU1aERqSoXfVJmm";
     }
 
-    function testSafeMintSingleAuthor() public {
-        sheets.safeMint("Me", "This", ipfsPath);
+    function testMintSingleContributor() public {
+        uint256 tokenId = sheets.mint("Me", "This", ipfsPath);
 
-        string author = sheets.authors(address(this))[0];
-        assertEq(author, "Me");
+        string memory contributor = sheets.getContributors(tokenId)[0];
+
+        assertEq(contributor, "Me");
+    }
+
+    function testMintMultipleContributors() public {
+        string[] memory input = new string[](3);
+        input[0] = "Me";
+        input[1] = "You";
+        input[2] = "Him";
+        uint256 tokenId = sheets.mint(input, "This", ipfsPath);
+
+        string[] memory contributors = sheets.getContributors(tokenId);
+
+        assertEq(contributors[0], "Me");
+        assertEq(contributors[1], "You");
+        assertEq(contributors[2], "Him");
+    }
+
+    function testMintTitle() public {
+        uint256 tokenId = sheets.mint("Me", "This", ipfsPath);
+
+        string memory title = sheets.getTitle(tokenId);
+
+        assertEq(title, "This");
+    }
+
+    function testMintUri() public {
+        uint256 tokenId = sheets.mint("Me", "This", ipfsPath);
+
+        string memory uri = sheets.tokenURI(tokenId);
+
+        assertEq(uri, ipfsPath);
+    }
+
+    function testMintIncrement() public {
+        uint256 firstTokenId = sheets.mint("Me", "This", ipfsPath);
+        uint256 secondTokenId = sheets.mint("Me", "This", ipfsPath);
+
+        assertTrue(firstTokenId == secondTokenId - 1);
     }
 }
